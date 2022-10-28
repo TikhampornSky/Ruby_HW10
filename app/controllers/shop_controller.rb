5d171b4a26b3dfd29d9cc8f6f448e284be7fc865
TikhampornSky <tikhamporntan@gmail.com>
2022-10-28 19:09:48 +0700

    add relation

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,6 +1,6 @@
 class ShopController < ApplicationController
     def shop
-        @user_id = params[:id]
+        @user_id = params[:id]              #send via url
         @myItem = Item.where(user_id: @user_id).all
     end
 end
