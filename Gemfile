8acac6e67dd3b53a8658a2fd73d3d0cf7e49792c
TikhampornSky <tikhamporntan@gmail.com>
2022-10-30 20:49:52 +0700

    Completly Finish

--- Gemfile
+++ Gemfile
@@ -46,10 +46,10 @@ gem "bootsnap", require: false
 # gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-gem "image_processing", ">= 1.2"
+gem "image_processing", "~> 1.2"
 
-gem 'mini_magick'
-gem 'carrierwave', '~> 2.0'
+# gem 'mini_magick'
+# gem 'carrierwave', '~> 2.0'
 gem 'bootstrap', '~> 5.2.2'
 
 group :development, :test do
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@ class Item < ApplicationRecord
     belongs_to :user
     has_many :inventories
 
-    has_one_attached :picture
-    # has_one_attached :picture do |attachable|     #It will make method name 'picture'
-    #     attachable.variant :thumb, resize_to_fit: [10, 10]
-    # end
+    #has_one_attached :picture
+    has_one_attached :picture do |attachable|
+        attachable.variant :thumb, resize_to_limit: [100, 100]
+    end
     
 end
